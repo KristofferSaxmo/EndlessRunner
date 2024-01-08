313d913598094d1528e7c6f627835342e9fedd23
KristofferSaxmo <54435541+KristofferSaxmo@users.noreply.github.com>
2024-01-08 16:41:51 +0100

    Finishing touches

--- Source/EndlessRunner/Private/Player2AIController.cpp
+++ Source/EndlessRunner/Private/Player2AIController.cpp
@@ -30,45 +30,43 @@ void APlayer2AIController::Tick(float DeltaSeconds)
 		FindDesiredPosition(TargetPlatform);
 	}
 
-	if (ControlledPawn->Position == DesiredPosition) return;
+	if (ControlledPawn->GetPosition() == DesiredPosition) return;
 
-	if (FMath::Abs(ControlledPawn->Position - DesiredPosition) > 1)
+	if (FMath::Abs(ControlledPawn->GetPosition() - DesiredPosition) > 1)
 		ControlledPawn->Jump();
 	
 	if (Timer < MoveCooldown) return;
 	
-	ControlledPawn->Position > DesiredPosition ? ControlledPawn->Move(true) : ControlledPawn->Move(false);
+	ControlledPawn->GetPosition() > DesiredPosition ? ControlledPawn->Move(true) : ControlledPawn->Move(false);
 	Timer = 0.0f;
 }
 
 void APlayer2AIController::FindDesiredPosition(const AMyPlatform* TargetPlatform)
 {
 	FreeLanes = TargetPlatform->NextPlatform->FreeLanes;
+	
 	if (FreeLanes.Num() == 0)
-	{
 		FindDesiredPosition(TargetPlatform->NextPlatform);
-	}
+	
 	else
-	{
 		DesiredPosition = FindClosestFreeLane();
-	}
 }
 
 int32 APlayer2AIController::FindClosestFreeLane() const
 {
-	const int32 CurrentPosition = ControlledPawn->Position;
-	int32 ClosestValue = FreeLanes[0];
-	int32 MinDifference = FMath::Abs(ClosestValue - CurrentPosition);
+	const int32 CurrentPosition = ControlledPawn->GetPosition();
+	int32 ClosestFreeLane = FreeLanes[0];
+	int32 MinDifference = FMath::Abs(ClosestFreeLane - CurrentPosition);
 
-	for (const int32 Val : FreeLanes)
+	for (const int32 Lane : FreeLanes)
 	{
-		const int32 CurrentDifference = FMath::Abs(Val - CurrentPosition);
+		const int32 CurrentDifference = FMath::Abs(Lane - CurrentPosition);
 		if (CurrentDifference < MinDifference)
 		{
 			MinDifference = CurrentDifference;
-			ClosestValue = Val;
+			ClosestFreeLane = Lane;
 		}
 	}
 
-	return ClosestValue;
+	return ClosestFreeLane;
 }
